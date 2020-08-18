de0331b4ac637f6c1a0819a976cea1636603842e
parkwoongki <sbe03005@naver.com>
2020-08-18 23:05:28 +0900

    BaekJoon_13

--- BaekJoon_13/src/BaekJoon_13_2231/BaekJoon_13_2231.java
+++ BaekJoon_13/src/BaekJoon_13_2231/BaekJoon_13_2231.java
@@ -1,4 +1,4 @@
-package BaekJoon_13_2798;
+package BaekJoon_13_2231;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,36 +7,33 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class BaekJoon_13_2798 {
+public class BaekJoon_13_2231 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(reader.readLine());
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		String s = st.nextToken();
+		int len = s.length();
+		int n = Integer.parseInt(s);
 
-		int[] arr = new int[n];
-		st = new StringTokenizer(reader.readLine());
 		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		int max = 0;
-		int tmp = 0;
-		for (int i = 0; i < n - 2; i++) {
-			for (int j = i + 1; j < n - 1; j++) {
-				for (int j2 = j + 1; j2 < n; j2++) {
-					tmp = arr[i] + arr[j] + arr[j2];
-					if (tmp > max && tmp <= m) {
-						max = tmp;
-					}
-				}
+			int con = i;
+			int tmp = i;
+			for (int j = 0; j < len; j++) {
+				con += tmp % 10;
+				tmp = tmp / 10;
+			}
+			if (con == n) {
+				writer.write(i + "");
+				writer.flush();
+				writer.close();
+				reader.close();
+				return;
 			}
 		}
-		writer.write(max + "");
-
+		writer.write(0 + "");
 		writer.flush();
 		writer.close();
 		reader.close();
